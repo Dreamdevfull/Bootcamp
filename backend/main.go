edaa80801d5da486339f92a587e95161a70342ee
Dreamdevfull <rinboy.gamer@gmail.com>
2026-03-15 19:59:24 +0700

    update backend

--- backend/main.go
+++ backend/main.go
@@ -4,8 +4,8 @@ import (
 	"github.com/Dreamdevfull/Bootcamp/config"
 	"github.com/Dreamdevfull/Bootcamp/container"
 	"github.com/Dreamdevfull/Bootcamp/routes"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v3"
-	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func main() {
@@ -13,7 +13,7 @@ func main() {
 	config.DatabaseConnected()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:2000",
+		AllowOrigins:     "http://localhost:3000",
 		AllowMethods:     "GET,POST,PUT,DELETE",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 		AllowCredentials: true,
