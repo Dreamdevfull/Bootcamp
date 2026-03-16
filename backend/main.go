4d03b2060eb7d235ae83794249062d367aae8e94
Anusit <anusit2547dream@gmail.com>
2026-03-16 12:33:20 +0700

    cors v3

--- backend/main.go
+++ backend/main.go
@@ -4,8 +4,9 @@ import (
 	"github.com/Dreamdevfull/Bootcamp/config"
 	"github.com/Dreamdevfull/Bootcamp/container"
 	"github.com/Dreamdevfull/Bootcamp/routes"
-	"github.com/gofiber/fiber/v2/middleware/cors"
+
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func main() {
@@ -13,9 +14,9 @@ func main() {
 	config.DatabaseConnected()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:2000",
-		AllowMethods:     "GET,POST,PUT,DELETE",
-		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
+		AllowOrigins:     []string{"http://localhost:2000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
 	}))
 
