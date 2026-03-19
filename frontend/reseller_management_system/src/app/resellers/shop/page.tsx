dbceaa432f6b97f8e523be313e7c61390797a950
Thanawat3781 <thanawat612612@gmail.com>
2026-03-19 21:11:22 +0700

    create button resellers shop

--- frontend/reseller_management_system/src/app/resellers/shop/page.tsx
+++ frontend/reseller_management_system/src/app/resellers/shop/page.tsx
@@ -9,37 +9,44 @@ import { ShopProducts as ShopProductsType } from '@/app/types/model';
 import { DataTable } from '@/app/components/ui/datatable';
 import { FilterSearchAndDropdown } from '@/app/components/ui/filter';
 
-const mockmain = {
-  text1: "จัดการหน้าร้าน",
-  text2: "สินค้าที่คุณกำลังขายในหน้าร้านคุณ"
-}
 
 const Shoppage = () => {
   const [data, setData] = useState<ShopProductsType[]>([]);
-    const [loading, setLoading] = useState(true);
-    const API_URL = process.env.NEXT_PUBLIC_API_URL;
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const res = await fetch(`${API_URL}/reseller/my-products`, {
-            credentials: "include",
-          });
-          const result = await res.json();
-          setData((result.data ?? []).sort((a: ShopProductsType, b: ShopProductsType) => b.id - a.id));
-        } catch {
-          setData(data);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchData();
-    },[]);
+  const [loading, setLoading] = useState(true);
+  const [open, setOpen] = useState(false)
+
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${API_URL}/reseller/my-products`, {
+          credentials: "include",
+        });
+        const result = await res.json();
+        setData((result.data ?? []).sort((a: ShopProductsType, b: ShopProductsType) => b.id - a.id));
+      } catch {
+        setData(data);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const mockmain = {
+    text1: "จัดการหน้าร้าน",
+    text2: "สินค้าที่คุณกำลังขายในหน้าร้านคุณ",
+    button: {
+      label: "ดูหน้าร้านค้า",
+      onClick: () => setOpen(true)
+    },
+  }
   return (
     <div className="min-h-screen bg-[#F5F3EE]">
-      <HeaderReseller/>
+      <HeaderReseller />
       <Main main={mockmain}/>
-      <FilterSearchAndDropdown/>
+      <FilterSearchAndDropdown />
       <div className='px-8 py-1'>
         {/* <ShopProductCrad data={data} loading={loading}/> */}
         <DataTable columns={ProductsColumn} data={data} loading={loading} />
