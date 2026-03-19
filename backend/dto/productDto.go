e6e9ee2b97cfad73c40a2871b29ea9744f508f6b
tonkar595 <tonkar595@gmail.com>
2026-03-19 20:44:45 +0700

    ีupdated products

--- backend/dto/productDto.go
+++ backend/dto/productDto.go
@@ -19,10 +19,10 @@ type ProductResponse struct {
 }
 
 type UpdateProductRequest struct {
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	ImageURL    string  `json:"image_url" validate:"required"`
-	CostPrice   float64 `json:"cost_price"`
-	MinPrice    float64 `json:"min_price"`
-	Stock       int     `json:"stock"`
+	Name        string  `json:"name" form:"name"`
+	Description string  `json:"description" form:"description"`
+	ImageURL    string  `json:"image_url" form:"image_url"`
+	CostPrice   float64 `json:"cost_price" form:"cost_price"`
+	MinPrice    float64 `json:"min_price" form:"min_price"`
+	Stock       *int    `json:"stock" form:"stock"`
 }
