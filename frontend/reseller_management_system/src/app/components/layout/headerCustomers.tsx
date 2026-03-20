5bd5a5635ebd4d1bb4e2a777a716537139a2b2da
Thanawat3781 <thanawat612612@gmail.com>
2026-03-21 02:29:31 +0700

    create basket page

--- frontend/reseller_management_system/src/app/components/layout/headerCustomers.tsx
+++ frontend/reseller_management_system/src/app/components/layout/headerCustomers.tsx
@@ -33,23 +33,10 @@ const HeaderCustomers = () => {
         <Link href={`/shop/${shop_slug}/tracking`}>
           <button className={sidebarLinkClass(`/shop/${shop_slug}/tracking`)}>ติดตามสินค้า</button>
         </Link>
-        <button type="button" className="flex items-center gap-2 bg-[#ef9f27] text-black border border-black px-6 py-2 rounded-lg hover:bg-[#BA7517] transition cursor-pointer">
-          <svg
-            className="w-5 h-5"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
-          <span className="text-[18px]">ตะกร้า (8) </span>
-        </button>
+        <Link href={`/shop/${shop_slug}/basket`}>
+        <button type="button" className="flex items-center gap-2 bg-[#ef9f27] text-white px-6 py-2 rounded-lg hover:bg-[#BA7517] transition cursor-pointer">
+          <span className="text-[18px]">🛒ตะกร้า (8) </span>
+        </button></Link>
       </div>
     </nav>
   )
